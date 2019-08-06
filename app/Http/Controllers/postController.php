86068025eba50fcee2f9c99664e640ad00b72fea
tanj1m <tanjim1337@gmail.com>
2019-08-06 14:29:11 +0600

    Added Data Validation

--- app/Http/Controllers/postController.php
+++ app/Http/Controllers/postController.php
@@ -34,7 +34,16 @@ class postController extends Controller
      */
     public function store(Request $request)
     {
+        
+
+        $this->validate($request,[
+            'title'=>'required|max:255',
+            'featured'=>'required|image',
+            'content'=>'required'
+        ]);
+
         dd($request->all());
+
     }
 
     /**
